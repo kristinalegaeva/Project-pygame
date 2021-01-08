af659aa7dbaa4f81bc04613572791057634526d7
kristinalegaeva <74817607+kristinalegaeva@users.noreply.github.com>
2021-01-08 17:12:47 +0500

    Update project.py
    game4 - лабиринт. поле для лабиринта и игрок, который по этому полю перемещается. 
    
    game3 - 2048. еще не работает

--- project.py
+++ project.py
@@ -97,15 +97,15 @@ def f_game1():
     fps_game1 = 6
     snake = [(15, 15), (16, 15), (17, 15)]
     vx, vy = -1, 0
-    apple = (random.randrange(0, 20), random.randrange(0, 20))
+    apple = (random.randrange(0, 25), random.randrange(0, 25))
     while apple in snake:
-        apple = (random.randrange(0, 20), random.randrange(0, 20))
+        apple = (random.randrange(0, 25), random.randrange(0, 25))
     clock = pygame.time.Clock()
     game1_sprites = pygame.sprite.Group()
     back = Button(load_image('back.jpg'), 600, 400)
     back.add(game1_sprites)
-    board = Board(20, 20, snake, apple)
-    board.set_view(100, 100, 20)
+    board = Board(25, 25, snake, apple)
+    board.set_view(50, 50, 20)
     end = False
 
     while True:
@@ -126,14 +126,14 @@ def f_game1():
                     vx, vy = -1, 0
                 elif event.key == pygame.K_DOWN and vx != -1:
                     vx, vy = 1, 0
-        if snake[0][0]  + vx == -1 or snake[0][1] + vy < 0 or snake[0][0] + vx > 19 or snake[0][1] + vy > 19 or len(snake) != len(set(snake)):
+        if snake[0][0]  + vx == -1 or snake[0][1] + vy < 0 or snake[0][0] + vx > 24 or snake[0][1] + vy > 24 or len(snake) != len(set(snake)):
             end = True
         if not end:
             if (snake[0][0] + vx, snake[0][1] + vy) == apple:
                 snake = [(snake[0][0] + vx, snake[0][1] + vy)] + snake[:]
-                apple = (random.randrange(0, 20), random.randrange(0, 20))
+                apple = (random.randrange(0, 25), random.randrange(0, 25))
                 while apple in snake:
-                    apple = (random.randrange(0, 20), random.randrange(0, 20))
+                    apple = (random.randrange(0, 25), random.randrange(0, 25))
                 now_snake += 10
                 if rec_snake < now_snake:
                     rec_snake = now_snake
@@ -147,7 +147,7 @@ def f_game1():
         string_rendered = font.render(f'{now_snake} / {rec_snake}', 1, pygame.Color('red'))
         intro_rect = string_rendered.get_rect()
         intro_rect.top = 50
-        intro_rect.x = 400
+        intro_rect.x = 600
 
         game1_sprites.draw(screen)
         board.render(snake, apple)
@@ -158,53 +158,6 @@ def f_game1():
 
 
 def f_game2():
-    clock = pygame.time.Clock()
-    game2_sprites = pygame.sprite.Group()
-    back = Button(load_image('back.jpg'), 600, 400)
-    back.add(game2_sprites)
-
-    while True:
-        screen.fill('#000000')
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                terminate()
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                coords = pygame.mouse.get_pos()
-                if back.rect.collidepoint(coords):
-                    return
-        game2_sprites.draw(screen)
-        pygame.display.flip()
-        clock.tick(FPS)
-
-def f_game3():
-    clock = pygame.time.Clock()
-    game3_sprites = pygame.sprite.Group()
-    back = Button(load_image('back.jpg'), 600, 450)
-    back.add(game3_sprites)
-    map_2048 = [[int(x) for x in line.strip().split()] for line in open(os.path.join('data', '2048', 'map.txt')).readlines()]
-    print(map_2048)
-    while True:
-        screen.fill('#000000')
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                terminate()
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                coords = pygame.mouse.get_pos()
-                if back.rect.collidepoint(coords):
-                    return
-        pygame.draw.rect(screen, (100, 100, 100), (60, 60, 480, 480))
-        game3_sprites.draw(screen)
-
-        for i in range(0, 6):
-            for j in range(0, 6):
-                #pygame.draw.rect(screen, (10, 10, 10), (60 + i * 80 + 1, 60 + j * 80 + 1, 78, 78))
-                #rect = load_image(f'2048/{map_2048[i][j]}.jpg').get_rect().move(60 + i * 80, 60 + j * 80)
-                screen.blit(load_image('2048', f'{map_2048[i][j]}.jpg'), (60 + i * 80, 60 + j * 80))
-
-        pygame.display.flip()
-        clock.tick(FPS)
-
-def f_game4():
     clock = pygame.time.Clock()
     game1_sprites = pygame.sprite.Group()
     back = Button(load_image('back.jpg'), 100, 100)
@@ -222,6 +175,213 @@ def f_game4():
         game1_sprites.draw(screen)
         pygame.display.flip()
         clock.tick(FPS)
+
+def f_game3():
+    class Tile(pygame.sprite.Sprite):
+        def __init__(self, tile_type, x, y):
+            super().__init__(tiles_group, game3_sprites)
+            self.type = tile_type
+            self.image = tile_images[tile_type]
+            self.coords = (x, y)
+            self.rect = self.image.get_rect().move(x, y)
+            self.stop = False
+
+        def collide(self, tiles, direction):
+            for tile in tiles:
+                xt, yt = tile.coords
+                x, y = self.coords
+                if direction == 'r':
+                    return y == yt and xt - x == 80 and not tile.stop
+                if direction == 'l':
+                    return y == yt and x - xt == 80  and not tile.stop
+                if direction == 'u':
+                    return x == xt and y - yt == 80 and not tile.stop
+                if direction == 'd':
+                    return x == xt and yt - y == 80 and not tile.stop
+
+
+        def update(self, e):
+            if e == pygame.K_RIGHT:
+                x, y = self.coords
+                self.stop = True if y == 460 else False
+                print(self.collide(tiles, 'r'), self.coords, self.type)
+                while self.collide(tiles, 'r') or not self.stop:
+
+                    #print(pygame.sprite.spritecollideany(self, tiles_group))
+                    x, y = self.coords
+                    x += 10
+                    self.rect = self.image.get_rect().move(x, y)
+                    self.coords = (x, y)
+                    self.stop = True if y == 460 else False
+                    tiles.clear()
+                    tiles.append(self)
+            if e == pygame.K_LEFT:
+                pass
+            if e == pygame.K_UP:
+                pass
+            if e == pygame.K_DOWN:
+                pass
+
+
+    class Fon(pygame.sprite.Sprite):
+        def __init__(self, x, y):
+            super().__init__(fon_group, game3_sprites)
+            self.type = 0
+            self.image = tile_images[0]
+            self.rect = self.image.get_rect().move(x, y)
+            self.coords = (x, y)
+
+
+    def left():
+        for i in range(6):
+            a = []
+            a_zero = []
+            for j in range(6):
+                if map_2048[i][j] != 0:
+                    a.append(map_2048[i][j])
+                else:
+                    a_zero.append(0)
+            map_2048[i] = a + a_zero
+
+    def right():
+        for i in range(6):
+            a = []
+            a_zero = []
+            for j in range(6):
+                if map_2048[i][j] != 0:
+                    a.append(map_2048[i][j])
+                else:
+                    a_zero.append(0)
+            map_2048[i] = a_zero + a
+
+    def up():
+        for j in range(6):
+            a = []
+            a_zero = []
+            for i in range(6):
+                if map_2048[i][j] != 0:
+                    a.append(map_2048[i][j])
+                else:
+                    a_zero.append(0)
+            for i in range(6):
+                a += a_zero
+                map_2048[i][j] = a[i]
+
+    def down():
+        for j in range(6):
+            a = []
+            a_zero = []
+            for i in range(6):
+                if map_2048[i][j] != 0:
+                    a.append(map_2048[i][j])
+                else:
+                    a_zero.append(0)
+            for i in range(6):
+                a_zero += a
+                map_2048[i][j] = a_zero[i]
+
+    fon_group = pygame.sprite.Group()
+    tiles_group = pygame.sprite.Group()
+    game3_sprites = pygame.sprite.Group()
+    tiles = []
+    clock = pygame.time.Clock()
+    back = Button(load_image('back.jpg'), 600, 450)
+    back.add(game3_sprites)
+    tile_images = {}
+    for x in [0, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]:
+        tile_images[x] = load_image('2048', f'{x}.jpg')
+    map_2048 = [[int(x) for x in line.strip().split()] for line in open(os.path.join('data', '2048', 'map.txt')).readlines()]
+    print(map_2048)
+    while True:
+        screen.fill('#000000')
+        tiles.clear()
+        for i in range(0, 6):
+            for j in range(0, 6):
+                pos = map_2048[j][i]
+                if pos == 0:
+                    fon = Fon(60 + i * 80, 60 + j * 80)
+                else:
+                    fon = Fon(60 + i * 80, 60 + j * 80)
+                    tile = Tile(pos, 60 + i * 80, 60 + j * 80)
+                    tiles.append(tile)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                coords = pygame.mouse.get_pos()
+                if back.rect.collidepoint(coords):
+                    return
+            elif event.type == pygame.KEYDOWN:
+                tiles_group.update(event.key)
+                #pygame.draw.rect(screen, (10, 10, 10), (60 + i * 80 + 1, 60 + j * 80 + 1, 78, 78))
+                #rect = load_image(f'2048/{map_2048[i][j]}.jpg').get_rect().move(60 + i * 80, 60 + j * 80)
+                #screen.blit(load_image('2048', f'{map_2048[i][j]}.jpg'), (60 + i * 80, 60 + j * 80))
+
+        #pygame.draw.rect(screen, '#303030', (50, 50, 500, 500))
+        game3_sprites.draw(screen)
+        fon_group.draw(screen)
+        tiles_group.draw(screen)
+
+
+        pygame.display.flip()
+        clock.tick(FPS)
+
+def f_game4():
+    class Wall(pygame.sprite.Sprite):
+        def __init__(self, tile_type, x, y):
+            super().__init__(walls_group, game4_sprites)
+            self.image = tile_images[tile_type]
+            self.rect = self.image.get_rect().move(x, y)
+
+    class Player(pygame.sprite.Sprite):
+        def __init__(self, x, y):
+            super().__init__(player_group, game4_sprites)
+            self.image = player_image
+            self.rect = self.image.get_rect().move(x, y)
+
+    clock = pygame.time.Clock()
+    game4_sprites = pygame.sprite.Group()
+    walls_group = pygame.sprite.Group()
+    player_group = pygame.sprite.Group()
+    back = Button(load_image('back.jpg'), 600, 400)
+    back.add(game4_sprites)
+    tile_images = {}
+    for x in range(16):
+        tile_images[x] = load_image('maze', f'{x}.jpg')
+    map_maze = [[int(x) for x in line.strip().split()] for line in
+                open(os.path.join('data', 'maze', 'map2.txt')).readlines()]
+    player_image = load_image('maze', 'k.jpg')
+    for i in range(len(map_maze)):
+        for j in range(len(map_maze[i])):
+            if map_maze[i][j] == -1:
+                Wall(0, 54 + j * 12, 54 + i * 12)
+                player = Player(54 + j * 12 - 0, 54 + i * 12 - 0)
+            else:
+                Wall(map_maze[i][j], 54 + j * 12, 54 + i * 12)
+    while True:
+        screen.fill('#000000')
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                coords = pygame.mouse.get_pos()
+                if back.rect.collidepoint(coords):
+                    return
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    player.rect.x += 12
+                if event.key == pygame.K_LEFT:
+                    player.rect.x -= 12
+                if event.key == pygame.K_UP:
+                    player.rect.y -= 12
+                if event.key == pygame.K_DOWN:
+                    player.rect.y += 12
+        game4_sprites.draw(screen)
+        player_group.draw(screen)
+        pygame.display.flip()
+        clock.tick(FPS)
+
 
 def f_records():
     screen.fill('#000000')
